62f26e0138262170b8bb3818054c22e97e883b2c
Zach Broniszewski <zbron99@gmail.com>
2019-07-14 17:21:26 -0400

    updated to parse as needed

--- index.js
+++ index.js
@@ -15,6 +15,8 @@
  */
 'use strict';
 
+const BN = require('bignumber.js');
+
 const MessageType = {
   VERSION: 'V',
   ORDER_ENTERED: 'E',
@@ -30,21 +32,9 @@ exports.Side = {
   SELL: 'S',
 };
 
-exports.format = (message) => {
-  switch (message.messageType) {
-    case MessageType.VERSION:
-      return formatVersion(message);
-    case MessageType.ORDER_ENTERED:
-      return formatOrderEntered(message);
-    case MessageType.ORDER_ADDED:
-      return formatOrderAdded(message);
-    case MessageType.ORDER_CANCELED:
-      return formatOrderCanceled(message);
-    case MessageType.TRADE:
-      return formatTrade(message);
-    default:
-      throw new Error('Unknown message type: ' + message.messageType);
-  }
+const Factor = {
+  SIZE: Math.pow(10, 8),
+  PRICE: Math.pow(10, 8),
 };
 
 exports.parse = (buffer) => {
@@ -66,35 +56,11 @@ exports.parse = (buffer) => {
   }
 };
 
-function formatVersion(message) {
-  const buffer = Buffer.allocUnsafe(5);
-
-  buffer.writeUInt8(0x56, 0);
-  buffer.writeUInt32BE(message.version, 1);
-
-  return buffer;
-}
-
 function parseVersion(buffer) {
   return {
     messageType: MessageType.VERSION,
     version: buffer.readUInt32BE(1),
   };
-}
-
-function formatOrderEntered(message) {
-  const buffer = Buffer.allocUnsafe(50);
-
-  buffer.writeUInt8(0x45, 0);
-  writeUInt64BE(buffer, message.timestamp, 1);
-  writeString(buffer, message.username, 9, 8);
-  writeUInt64BE(buffer, message.orderNumber, 17);
-  writeString(buffer, message.side, 25, 1);
-  writeString(buffer, message.instrument, 26, 8);
-  writeUInt64BE(buffer, message.quantity, 34);
-  writeUInt64BE(buffer, message.price, 42);
-
-  return buffer;
 }
 
 function parseOrderEntered(buffer) {
@@ -103,21 +69,11 @@ function parseOrderEntered(buffer) {
     timestamp: readUInt64BE(buffer, 1),
     username: readString(buffer, 9, 8),
     orderNumber: readUInt64BE(buffer, 17),
-    side: readString(buffer, 25, 1),
-    instrument: readString(buffer, 26, 8),
-    quantity: readUInt64BE(buffer, 34),
-    price: readUInt64BE(buffer, 42),
+    side: parseOrderSide(readString(buffer, 25, 1)),
+    instrument: trim(readString(buffer, 26, 8)),
+    quantity: parseOrderQuantity(readUInt64BE(buffer, 34)),
+    price: parseOrderPrice(readUInt64BE(buffer, 42)),
   };
-}
-
-function formatOrderAdded(message) {
-  const buffer = Buffer.allocUnsafe(17);
-
-  buffer.writeUInt8(0x41, 0);
-  writeUInt64BE(buffer, message.timestamp, 1);
-  writeUInt64BE(buffer, message.orderNumber, 9);
-
-  return buffer;
 }
 
 function parseOrderAdded(buffer) {
@@ -128,37 +84,13 @@ function parseOrderAdded(buffer) {
   };
 }
 
-function formatOrderCanceled(message) {
-  const buffer = Buffer.allocUnsafe(25);
-
-  buffer.writeUInt8(0x58, 0);
-  writeUInt64BE(buffer, message.timestamp, 1);
-  writeUInt64BE(buffer, message.orderNumber, 9);
-  writeUInt64BE(buffer, message.canceledQuantity, 17);
-
-  return buffer;
-}
-
 function parseOrderCanceled(buffer) {
   return {
     messageType: MessageType.ORDER_CANCELED,
     timestamp: readUInt64BE(buffer, 1),
     orderNumber: readUInt64BE(buffer, 9),
-    canceledQuantity: readUInt64BE(buffer, 17),
+    canceledQuantity: parseOrderQuantity(readUInt64BE(buffer, 17)),
   };
-}
-
-function formatTrade(message) {
-  const buffer = Buffer.allocUnsafe(37);
-
-  buffer.writeUInt8(0x54, 0);
-  writeUInt64BE(buffer, message.timestamp, 1);
-  writeUInt64BE(buffer, message.restingOrderNumber, 9);
-  writeUInt64BE(buffer, message.incomingOrderNumber, 17);
-  writeUInt64BE(buffer, message.quantity, 25);
-  buffer.writeUInt32BE(message.matchNumber, 33);
-
-  return buffer;
 }
 
 function parseTrade(buffer) {
@@ -167,14 +99,25 @@ function parseTrade(buffer) {
     timestamp: readUInt64BE(buffer, 1),
     restingOrderNumber: readUInt64BE(buffer, 9),
     incomingOrderNumber: readUInt64BE(buffer, 17),
-    quantity: readUInt64BE(buffer, 25),
+    quantity: parseOrderQuantity(readUInt64BE(buffer, 25)),
     matchNumber: buffer.readUInt32BE(33),
   };
 }
 
-function writeUInt64BE(buffer, value, offset) {
-  buffer.writeUInt32BE(value / 0x100000000, offset);
-  buffer.writeUInt32BE(value % 0x100000000, offset + 4);
+function parseOrderSide(side) {
+  const parse = {
+    B: 'buy',
+    S: 'sell',
+  };
+  return parse[side];
+}
+
+function parseOrderQuantity(quantity) {
+  return new BN(quantity).dividedBy(Factor.SIZE).toString();
+}
+
+function parseOrderPrice(price) {
+  return new BN(price).dividedBy(Factor.PRICE).toString();
 }
 
 function readUInt64BE(buffer, offset) {
@@ -182,12 +125,6 @@ function readUInt64BE(buffer, offset) {
   const low = buffer.readUInt32BE(offset + 4);
 
   return 0x100000000 * high + low;
-}
-
-function writeString(buffer, value, offset, length) {
-  const count = buffer.write(value, offset, length, 'ascii');
-
-  buffer.fill(0x20, offset + count, offset + length);
 }
 
 function readString(buffer, offset, length) {
